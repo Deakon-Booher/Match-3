c7b70c59ad12961866b6c4bbd3d82a49eb39bf47
Deakon-Booher <dwbooher@iu.edu>
2022-03-01 22:42:25 -0500

    done with project

--- Baground Container.gd
+++ Baground Container.gd
@@ -3,8 +3,8 @@ extends Node2D
 var score_threshold = 100
 var b = 0
 var backgrounds = [
-	preload("res://Assets/nature background.png")
-	,preload("res://Assets/town background.png")
+	preload("res://Assets/town background.png")
+	,preload("res://Assets/nature background.png")
 	,preload("res://Assets/castle background.png")
 	
 ]
@@ -13,6 +13,7 @@ func _physics_process(delta):
 	if Global.score > score_threshold*(b+1):
 		var B2 = Sprite.new()
 		B2.centered = false
+		B2.scale = Vector2(0.5,0.5)
 		B2.texture = backgrounds[wrapi(b, 0, backgrounds.size())]
 		B2.name = "B2"
 		add_child(B2)
